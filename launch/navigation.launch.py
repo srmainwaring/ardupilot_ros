3ca3490151d55d166d433e58ca83c3031d50f09e
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-09-27 16:26:43 +0100

    nav2: Copter: Add SDF_PATH to environment in navigation launch
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -1,3 +1,5 @@
+import os
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction
 from launch.substitutions import LaunchConfiguration
@@ -37,6 +39,19 @@ def generate_launch_description():
             ),
         ]
     )
+
+    # Robot description.
+
+    # Ensure `SDF_PATH` is populated as `sdformat_urdf`` uses this rather
+    # than `GZ_SIM_RESOURCE_PATH` to locate resources.
+    if "GZ_SIM_RESOURCE_PATH" in os.environ:
+        gz_sim_resource_path = os.environ["GZ_SIM_RESOURCE_PATH"]
+
+        if "SDF_PATH" in os.environ:
+            sdf_path = os.environ["SDF_PATH"]
+            os.environ["SDF_PATH"] = sdf_path + ":" + gz_sim_resource_path
+        else:
+            os.environ["SDF_PATH"] = gz_sim_resource_path
 
     # RViz.
     rviz = Node(
